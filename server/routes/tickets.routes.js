e4eee7bb632f7e77710041a54945649c8fe302c1
Rafa <rafaelserranocardenas@gmail.com>
2020-04-13 19:47:20 +0200

    Ticket
    Editar/Eliminar/Buscar por id Ticket

--- server/routes/tickets.routes.js
+++ server/routes/tickets.routes.js
@@ -9,11 +9,28 @@ router.get('/', async (req, res) => {
     res.json(tickets); //los envia como respuesta
 });
 
+router.get('/:id', async (req, res) => {
+    const tickets = await Ticket.findById(req.params.id);
+    res.json(tickets);
+});
+
 router.post('/', async (req, res) => {
     const { listaProductos, total, fecha, efectivo, cambio, barra } =  req.body; //del body guarda los parametros indicados
     const ticket = new Ticket({ listaProductos, total, fecha, efectivo, cambio, barra }); //crea un nuevo ticket con esos parametros
     await ticket.save(); //almacena el ticket en la base de datos
     res.json({status: 'Ticket creado'}); //respuesta de ticket creado
+});
+
+router.put('/:id', async (req, res) => {
+    const { listaProductos, total, fecha, efectivo, cambio, barra } = req.body;
+    const nuevoticket = { listaProductos, total, fecha, efectivo, cambio, barra };
+    await Ticket.findByIdAndUpdate(req.params.id, nuevoticket); //busca por id y actualiza
+    res.json({status: 'Ticket actualizado'}); //respuesta de ticket actualizado
+});
+
+router.delete('/:id', async (req, res) => {
+    await Ticket.findByIdAndDelete(req.params.id); //busca por id y elimina
+    res.json({status: 'Ticket eliminado'}); //respuesta de ticket eliminado
 });
 
 module.exports = router;
