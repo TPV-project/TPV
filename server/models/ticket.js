15956c12116f2c84b01f8a58bdb13b4fa6f6ea79
Rafa <rafaelserranocardenas@gmail.com>
2020-04-14 18:37:43 +0200

    Array en module/ticket.js
    Añadir el array a la lista de productos

--- server/models/ticket.js
+++ server/models/ticket.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const TicketSchema =  new Schema({
-    listaProductos : {type: String, required: true},//Cambiar a vector de [producto,cantidad]
+    listaProductos : {type: Array, required: true},//Cambiar a vector de [producto,cantidad]
     total: {type: Number, required: true},
     fecha: {type: Date, required: true, default: Date.now()},
     efectivo: {type: Boolean, required: true},
